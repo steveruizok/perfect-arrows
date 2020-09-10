248907b88f09a4d950b29b1b61df6cf9c34b62dc
Harry Brundage <harry.brundage@gmail.com>
2020-09-10 14:18:55 -0400

    Add a regression test "suite"
    While fiddling with various pieces of the algorithms it's nice to know you haven't broken anything, and which situations have changed outputs. This adds a really lightweight test suite that ensures the APIs always work, and that changes to what arrows are being rendered at least require a snapshot update. It doesn't enforce that the arrows are correct or look perfect, but it at least arms developers with the knowledge of what they are changing so they can inspect it or fix it if it's unexpected.

---
+++ test/getArrow.test.ts
@@ -0,0 +1,16 @@
+import { getArrow} from "../src"
+
+describe("getArrow", () => {
+  test("it computes an arrow between points", () => {
+    expect(getArrow(10, 10, 30, 30)).toMatchSnapshot()
+  })
+
+  test("it computes an arrow from a point back to itself", () => {
+    expect(getArrow(10, 10, 10, 10)).toMatchSnapshot()
+  })
+
+  test("it computes a bowwed arrow between points", () => {
+    expect(getArrow(10, 10, 30, 11, {bow: 1})).toMatchSnapshot()
+  })
+
+})
